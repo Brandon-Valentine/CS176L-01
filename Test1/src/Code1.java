02416b3c415960a6c43fba0d0463b39e42929717
Brandon-Valentine <91150793+Brandon-Valentine@users.noreply.github.com>
2022-09-12 17:20:26 -0400

    Add files via upload

--- Test1/src/Code1.java
+++ Test1/src/Code1.java
@@ -2,7 +2,7 @@
 public class Code1 {
 
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		System.out.println("My name is Brandon Valentine");
 		// TODO Auto-generated method stub
 
 	}
