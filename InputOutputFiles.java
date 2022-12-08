df5eb42ffdab56cb62454840ac1867649e526315
Brandon-Valentine <91150793+Brandon-Valentine@users.noreply.github.com>
2022-12-08 15:16:52 -0500

    Add files via upload

---
+++ InputOutputFiles.java
@@ -0,0 +1,35 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+/**
+   Used to test the BlankLinesRemover class.
+*/
+public class InputOutputFiles
+{
+   public static void main(String[] args) throws FileNotFoundException
+   {    Scanner input= new Scanner(System.in);
+   boolean valid=false;
+   String path="";
+   
+   while(!valid) {
+	   System.out.println("Please enter the path to open the input file for example C:\\\\Downloads\\\\lines.txt ");
+	   path=(input.next());
+   try { 
+		   File inputFile = new File("lines.txt");
+		   System.out.println(inputFile);
+		   PrintWriter outputFile = new PrintWriter("outlines.txt");
+		   Scanner in = new Scanner(inputFile);
+		   while (in.hasNext()) 
+		   {
+			   String what =in.next();
+			   outputFile.println(what);
+			   System.out.println(what);
+			   }
+		   in.close();
+		   outputFile.close();}
+   catch (FileNotFoundException exception){
+	   System.out.println("File not found, please try again");
+   }}}}
+
